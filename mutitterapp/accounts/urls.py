bed448154bd6d3885c2480079b22571763535d03
anush.23 <anushitik@gmail.com>
2023-07-31 19:54:12 +0400

    reload

--- mutitterapp/accounts/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-from .views import signup_login
-
-urlpatterns = [
-    path('', signup_login, name="signup_login"),
-]
--- mutitterapp/accounts/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def signup_login(request):
-    return render(request, "signup_login.html")
