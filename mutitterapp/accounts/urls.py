08fff42fff5a8b87d2de625e8d6351f1d565321e
anush.23 <anushitik@gmail.com>
2023-08-02 01:42:01 +0400

    template with register

--- mutitterapp/accounts/urls.py
+++ mutitterapp/accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
-from .views import signup_login
+from .views import Register, signup_login
 
 urlpatterns = [
+    path('', include('django.contrib.auth.urls')),
     path('', signup_login, name="signup_login"),
+    path('register', Register.as_view(), name='register'),
 ]
--- mutitterapp/accounts/views.py
+++ mutitterapp/accounts/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from .forms import RegisterForm
 
 def signup_login(request):
-    return render(request, "signup_login.html")
+    return render(request, "register.html")
+
+class Register(CreateView):
+    form_class = RegisterForm
+    success_url = reverse_lazy("login")
+    template_name = "register.html" 
+""" Lizayi mot register.html a"""
+
+
+
 
 # Create your views here.
