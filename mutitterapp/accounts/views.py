e1ada1787608a5e0d962b70e46ab3a91157c7d24
asryanani1997 <asryan.ani1997@gmail.com>
2023-08-01 12:14:49 +0400

    Log in

--- mutitterapp/accounts/views.py
+++ mutitterapp/accounts/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def signup_login(request):
     return render(request, "signup_login.html")
+
+def home(request):
+    return HttpResponse("home/")
 
 # Create your views here.
