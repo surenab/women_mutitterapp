ee625ec9393599aa17983c41566940c31c3349f2
hliza84 <hovhannisyanliza84@gmail.com>
2023-08-02 17:16:16 +0400

    back to preview version

--- mutitterapp/accounts/urls.py
+++ mutitterapp/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .views import Register, signup_login
 
 urlpatterns = [
-    path('', include('django.contrib.auth.urls')),
-    path('', signup_login, name="signup_login"),
-    path('register', Register.as_view(), name='register'),
-
+    path("", include("django.contrib.auth.urls")),
+    path("", signup_login, name="signup_login"),
+    path("register", Register.as_view(), name="register"),
+]
