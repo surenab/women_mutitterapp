f3222db4124be72b7b6ef94ff497d8567df72775
anush.23 <anushitik@gmail.com>
2023-08-03 14:30:12 +0400

    new initial

---
+++ MUTITTERAPP/accounts/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from .forms import RegisterForm
+
+class Register(CreateView):
+    form_class = RegisterForm
+    success_url = reverse_lazy('login')
+    template_name = "registration/register.html" 
