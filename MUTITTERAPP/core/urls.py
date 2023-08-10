44443ee589ed2762add28da2a52a9a9174d7e50a
varsenikharutyunyane <varsikharutyunyan72@gmail.com>
2023-08-10 21:26:15 +0400

    Added MyKlingupdate

--- MUTITTERAPP/core/urls.py
+++ MUTITTERAPP/core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home, CreateKling
+from .views import home, CreateKling,MyKlingUpdate
 
 
 urlpatterns = [
     path("", home, name="home"),
     path("create-kling", CreateKling.as_view(), name="create_kling"),
+    path("my-klings/update/<int:pk>", MyKlingUpdate.as_view(), name="my_kling_update"),
 ]
--- MUTITTERAPP/core/views.py
+++ MUTITTERAPP/core/views.py
@@ -2,9 +2,12 @@
 from django.forms.models import BaseModelForm
 from django.shortcuts import render
 from .models import Kling
-from django.views.generic import CreateView
+from django.views.generic import CreateView,UpdateView
 from .forms import KlingForm
 from django.urls import reverse_lazy
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 
 def home(request):
@@ -21,4 +24,21 @@ class CreateKling(CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+    
+    
+class MyKlingUpdate(LoginRequiredMixin, UpdateView):
+    model = Kling
+    context_object_name = "kling"
+    form_class = KlingForm
+    success_url = reverse_lazy("my_klings")
+
+    def get_queryset(self):
+        queryset = super(MyKlingUpdate, self).get_queryset()
+        queryset = queryset.filter(user=self.request.user)
+        return queryset
+
+    def form_valid(self, form):
+        messages.success(self.request, "Kling instance is updated!")
+        return super().form_valid(form)
+
     
