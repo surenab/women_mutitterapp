72284eec0f57a5602d490ea8cacaaea69d3d95e1
surenabrahamyan <suren.abrahamyan@toptal.com>
2023-08-15 09:37:19 +0100

    cleaned up

--- MUTITTERAPP/core/urls.py
+++ MUTITTERAPP/core/urls.py
@@ -1,16 +1,14 @@
 from django.urls import path
 
-from .views import home, CreateKling, MyKling
+from .views import Home, CreateKling, MyKling
 
 from django.conf import settings
 
 from django.conf.urls.static import static
 
 
-
-
 urlpatterns = [
-    path("", home, name="home"),
+    path("", Home.as_view(), name="home"),
     path("create-kling", CreateKling.as_view(), name="create_kling"),
     path("my-kling", MyKling.as_view(), name="my_kling"),
 ]
