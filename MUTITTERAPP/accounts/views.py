cb5564a3f79143dc0b6a6aea78e1ae0dc8876e70
anush.23 <anushitik@gmail.com>
2023-08-29 02:31:21 +0400

    edited  message tags

--- MUTITTERAPP/accounts/views.py
+++ MUTITTERAPP/accounts/views.py
@@ -2,6 +2,19 @@ from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from .forms import RegisterForm
 from django.contrib import messages
+from django.contrib.auth.views import LoginView
+from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
+
+def custom_login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            pass
+    else:
+        form = AuthenticationForm(request)
+
+    return render(request, 'login.html', {'form': form})
 
 class Register(CreateView):
     form_class = RegisterForm
